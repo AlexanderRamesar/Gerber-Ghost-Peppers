ab4f35c59daade44aea669cbffefd27167d923d8
AlexanderRamesar <72363114+AlexanderRamesar@users.noreply.github.com>
2021-02-20 19:18:45 -0500

    Add files via upload

---
+++ Meikeslife.ino
@@ -0,0 +1,126 @@
+#include <ESP8266WiFi.h>
+
+
+const char* ssid     = "fau";
+const char* password = "";
+const char* host = "http://meikembuettner.com/";
+
+
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+void setup()
+{
+  
+    Serial.begin(115200);
+ 
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+
+void loop()
+{
+
+  /*
+float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();  
+  if(isnan(temperature) || isnan(humidity)){
+    Serial.println("Failed to read DHT11");
+  }else{
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(3000);
+  }
+  */
+
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   float volt = (peakToPeak * 5.0) / 1024;  // convert to volts
+   volt = volt * 200;
+   
+
+   Serial.println(volt);
+   
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+ 
+
+
+    // This will send the request to the server
+ client.print(String("GET http://meikembuettner.com/ScreamMachine/includes/dbh-inc.php?") + 
+                          ("&volt=") + volt +                   
+                          " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 1000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+}
